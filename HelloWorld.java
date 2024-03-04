b854c233e7057237d7b3d30b9815339247b7716b
daniel <dchimenoperez@safareyes.com>
2024-03-04 10:39:37 +0100

    Metodo fecha

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
     }
+    import java.util.Date;
+
+    public class Main {
+        public static void main(String[] args) {
+            Date fechaActual = new Date();
+            System.out.println("Fecha actual: " + fechaActual);
+        }
+    }
+
 }
